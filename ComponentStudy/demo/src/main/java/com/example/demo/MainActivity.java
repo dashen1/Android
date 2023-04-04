9fb9b9640dd8616dc99cb51178496c841b22ef3a
“na <l>
2023-04-04 13:51:56 +0800

    modified.

--- ComponentStudy/demo/src/main/java/com/example/demo/MainActivity.java
+++ ComponentStudy/demo/src/main/java/com/example/demo/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.demo;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
@@ -34,8 +37,63 @@ public class MainActivity extends AppCompatActivity {
             dataBinding.setMValue(finalString);
         });
 
-
     }
+
+    private int volumeUp = 0;
+    private int volumeDown = 0;
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+//        switch (keyCode){
+//            case KeyEvent.KEYCODE_VOLUME_UP:
+//                //音量键up
+//                Toast.makeText(this,"升高音量", Toast.LENGTH_SHORT).show();
+//            case KeyEvent.KEYCODE_VOLUME_DOWN:
+//                //音量键down
+//                Toast.makeText(this,"降低音量", Toast.LENGTH_SHORT).show();
+//            default:
+//                break;
+//        }
+        if (keyCode == KeyEvent.KEYCODE_VOLUME_UP) {
+            volumeUp++;
+        } else if (keyCode == KeyEvent.KEYCODE_VOLUME_DOWN) {
+            volumeDown++;
+        }
+        if (volumeUp == 1 && volumeDown == 1) {
+            Log.d("TAG", "组合键");
+            volumeUp = 0;
+            volumeDown = 0;
+            Toast.makeText(this, "音量组合键", Toast.LENGTH_SHORT).show();
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    // 监听遥控器的左键和右键，当连续按下左左右右后，弹出Toast
+    private long liftTime = 0;
+    private long rightTime = 0;
+    int leftCount = 0;
+    int rightCount = 0;
+
+//    @Override
+//    public boolean onKeyDown(int keyCode, KeyEvent event) {
+//        if (keyCode==KeyEvent.KEYCODE_DPAD_LEFT && event.getAction() == KeyEvent.ACTION_DOWN){
+//            leftCount++;
+//            liftTime = System.currentTimeMillis();
+//        }else if (keyCode==KeyEvent.KEYCODE_DPAD_RIGHT && event.getAction() == KeyEvent.ACTION_DOWN){
+//            rightCount++;
+//            rightTime = System.currentTimeMillis();
+//        }
+//        Log.e("TAG","rightTime-liftTime = "+(rightTime-liftTime));
+//        Log.e("TAG","leftCount = "+leftCount);
+//        Log.e("TAG","rightCount"+rightCount);
+//
+//        if (rightTime-liftTime < 2000 && leftCount == 2 && rightCount == 2){
+//            leftCount = 0;
+//            rightCount = 0;
+//            Toast.makeText(this,"退出应用", Toast.LENGTH_SHORT).show();
+//        }
+//        return super.onKeyDown(keyCode, event);
+//    }
 
     public static int getRandomInRange(int range) {
         return (int) (1 + Math.random() * (range));
