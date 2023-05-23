8b76e2b2ba7f24cf3000637d667bde0e3fdc4637
“na <l>
2023-05-23 17:52:22 +0800

    modified.

---
+++ ComponentStudy/tinker/src/main/java/com/vtech/mobile/tinker/app/BuildConfig.java
@@ -0,0 +1,14 @@
+package com.vtech.mobile.tinker.app;
+
+public final class BuildConfig {
+    public static final boolean DEBUG = Boolean.parseBoolean("true");
+    public static final String APPLICATION_ID = "tinker.sample.android";
+    public static final String BUILD_TYPE = "debug";
+    public static final String FLAVOR = "";
+    public static final int VERSION_CODE = 1;
+    public static final String VERSION_NAME = "1.0.0";
+    // Fields from default config.
+    public static final String MESSAGE = "I am the base apk";
+    public static final String PLATFORM = "all";
+    public static final String TINKER_ID = "1.0";
+}
