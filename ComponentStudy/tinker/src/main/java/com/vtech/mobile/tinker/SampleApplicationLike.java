9836df7a95b87011b25d710e1466514de876b787
“na <l>
2023-05-24 17:45:31 +0800

    modified.

--- ComponentStudy/tinker/src/main/java/com/vtech/mobile/tinker/SampleApplicationLike.java
+++ ComponentStudy/tinker/src/main/java/com/vtech/mobile/tinker/SampleApplicationLike.java
@@ -33,6 +33,8 @@ import com.vtech.mobile.tinker.util.TinkerManager;
 public class SampleApplicationLike extends DefaultApplicationLike {
     private static final String TAG = "Tinker.SampleApplicationLike";
 
+    private Tinker mTinker;
+
     public SampleApplicationLike(Application application, int tinkerFlags, boolean tinkerLoadVerifyFlag,
                                  long applicationStartElapsedTime, long applicationStartMillisTime, Intent tinkerResultIntent) {
         super(application, tinkerFlags, tinkerLoadVerifyFlag, applicationStartElapsedTime, applicationStartMillisTime, tinkerResultIntent);
@@ -66,6 +68,35 @@ public class SampleApplicationLike extends DefaultApplicationLike {
         //or you can put com.tencent.tinker.** to main dex
         TinkerManager.installTinker(this);
         Tinker tinker = Tinker.with(getApplication());
+
+        // 分包需要的东西
+//        MultiDex.install(base);
+//
+//        // LoadReporter类定义了Tinker在加载补丁时的一些回调
+//        LoadReporter loadReporter = new DefaultLoadReporter(getApplication());
+//        // PatchReporter类定义了Tinker在修复或者升级补丁时的一些回调
+//        PatchReporter patchReporter = new DefaultPatchReporter(getApplication());
+//        // PatchListener类是用来过滤Tinker收到的补丁包的修复、升级请求，也就是决定我们是不是真的要唤起:patch进程去尝试补丁合成。
+//        PatchListener patchListener = new DefaultPatchListener(getApplication());
+//        // UpgradePatch类是用来升级当前补丁包的处理类，一般来说你也不需要复写它。
+//        AbstractPatch upgradePatchProcessor = new UpgradePatch();
+//
+//        TinkerInstaller.install(this,
+//                loadReporter, patchReporter, patchListener,
+//                DefaultTinkerResultService.class, upgradePatchProcessor);
+
+//        MultiDex.install(base);
+//
+//        TinkerManager.setTinkerApplicationLike(this);
+//        // 设置全局异常捕获
+//        TinkerManager.initFastCrashProtect();
+//        //开启升级重试功能（在安装Tinker之前设置）
+//        TinkerManager.setUpgradeRetryEnable(true);
+//        //设置Tinker日志输出类
+//        TinkerInstaller.setLogIml(new MyLogImp());
+//        //安装Tinker(在加载完multiDex之后，否则你需要将com.tencent.tinker.**手动放到main dex中)
+//        TinkerManager.installTinker(this);
+//        mTinker = Tinker.with(getApplication());
     }
 
     @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
