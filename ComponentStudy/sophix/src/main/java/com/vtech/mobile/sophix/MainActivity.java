e68f64376f127d8d3363add644a83b7f4fc38c3d
“na <l>
2023-05-26 16:04:20 +0800

    modified.

--- ComponentStudy/sophix/src/main/java/com/vtech/mobile/sophix/MainActivity.java
+++ ComponentStudy/sophix/src/main/java/com/vtech/mobile/sophix/MainActivity.java
@@ -1,20 +1,53 @@
 package com.vtech.mobile.sophix;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_EXTERNAL_STORAGE_PERMISSION = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if (Build.VERSION.SDK_INT >= 23) {
+            requestExternalStoragePermission();
+        }
+
         TextView tv = findViewById(R.id.tv);
-        tv.setText("Bug 第二次！！");
-        tv.setTextColor(Color.RED);
+        tv.setText("Bug release 修复 1");
+        tv.setTextColor(Color.YELLOW);
+    }
+
+    private void requestExternalStoragePermission() {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                    REQUEST_EXTERNAL_STORAGE_PERMISSION);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQUEST_EXTERNAL_STORAGE_PERMISSION:
+                if (grantResults.length <= 0 || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(MainActivity.this, "OK", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+        }
     }
 }
--- ComponentStudy/sophix/src/main/java/com/vtech/mobile/sophix/MyRealApplication.java
+++ ComponentStudy/sophix/src/main/java/com/vtech/mobile/sophix/MyRealApplication.java
@@ -3,11 +3,13 @@ package com.vtech.mobile.sophix;
 import android.app.Application;
 import android.util.Log;
 
+import com.taobao.sophix.SophixManager;
+
 public class MyRealApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-
+        SophixManager.getInstance().queryAndLoadNewPatch();
         Log.i("MainApplication", "原有的Application，可以通过补丁修改");
     }
 
